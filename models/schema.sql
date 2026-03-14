a743e2325d8f2c603e323871f310c7eda90a423c
zeno <zenoindrive@gmail.com>
2026-03-14 09:30:23 +0530

    added transaction trigger

--- models/schema.sql
+++ models/schema.sql
@@ -1,28 +1,61 @@
 CREATE TABLE IF NOT EXISTS 'users'(
     'id' INTEGER PRIMARY KEY,
     'name' TEXT NOT NULL
-    'activated' TEXT CHECK(activated==1 or activated==0) DEFAULT 1
+    'activated' TEXT NOT NULL CHECK(activated==1 or activated==0) DEFAULT 1
     'created_at' NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP 
-)
+);
 
 CREATE TABLE IF NOT EXISTS 'banks'(
     'id' INTEGER PRIMARY KEY,
     'name' TEXT NOT NULL
 
-) 
+);
+
 CREATE TABLE IF NOT EXISTS 'accounts'(
     'id' INTEGER PRIMARY KEY,
-    'user_id' INTEGER,
-    'bank_id' INTEGER,
-    'activated' INTEGER CHECK(activated==1 or activated==0) DEFAULT 1,
+    'user_id' INTEGER NOT NULL,
+    'bank_id' INTEGER NOT NULL,
+    'account_type' INTEGER NOT NULL,
+    'balance' INTEGER NOT NULL CHECK('balance'>0),
+    'activated' INTEGER NOT NULL (activated==1 or activated==0) DEFAULT 1,
     'created_at' NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY('user_id' REFERENCES 'user'('id'),
     'bank_id' REFERENCES 'banks'('id') )
-)
+);
 
 CREATE TABLE IF NOT EXISTS 'transaction_history'(
     'id' INTEGER PRIMARY KEY AUTO_INCREMENT,
     'from_id' INTEGER NOT NULL,
     'to_id' INTEGER NOT NULL,
-    'transaction_time' NUMERIC DEFAULT CURRENT_TIMESTAMP
-)
+    'amount' INTEGER NOT NULL,
+    'transaction_time' NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX 'user_index' IF NOT EXISTS ON 'users'('name');
+CREATE INDEX 'bank_index' IF NOT EXISTS ON 'banks'('name');
+CREATE INDEX 'accounts_index' IF NOT EXISTS ON 'accounts'('bank_id,user_id');
+
+
+CREATE TRIGGER IF NOT EXISTS 'transactions'
+BEFORE INSERT ON 'transaction_history'
+FOR EACH ROW
+BEGIN
+    SELECT CASE 
+        WHEN(SELECT 'activated' FROM 'accounts' WHERE 'id'=NEW.'from_id')=0
+            THEN RAISE(ABORT,'ERROR:Sending account not active')
+        WHEN(SELECT 'activated' FROM 'accounts' WHERE 'id'=NEW.'to_id')=0
+            THEN RAISE(ABORT,'ERROR:Reciveing account not active')
+        WHEN(SELECT 'balance' FROM 'accounts' WHERE 'id'=NEW.'from_id')<NEW.'amount'
+            THEN RAISE(ABORT,'ERROR:Sending account has insufficient funds')
+    END;
+    UPDATE 'accounts'
+    SET 'balance'='balance'-NEW.'amount'
+    WHERE 'id'=NEW.'from_id';
+    
+    UPDATE 'accounts'
+    SET 'balance'='balance'+NEW.'amount'
+    WHERE 'id'=NEW.'to_id';
+END;
+
+
+
